0d3fa26dc22c9a55018dae41c6113330752adf26
Exho1 <nat.adams149@gmail.com>
2015-04-02 15:33:34 -0500

    Created

---
+++ Galaga/Main Form.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.updateTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.dbg = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtScore = New System.Windows.Forms.Label()
+        Me.startDelay = New System.Windows.Forms.Timer(Me.components)
+        Me.player = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblGameOver = New System.Windows.Forms.Label()
+        CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'updateTimer
+        '
+        Me.updateTimer.Enabled = True
+        Me.updateTimer.Interval = 10
+        '
+        'dbg
+        '
+        Me.dbg.AutoSize = True
+        Me.dbg.BackColor = System.Drawing.Color.White
+        Me.dbg.Location = New System.Drawing.Point(682, 552)
+        Me.dbg.Name = "dbg"
+        Me.dbg.Size = New System.Drawing.Size(39, 13)
+        Me.dbg.TabIndex = 2
+        Me.dbg.Text = "Label1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 31)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Score:"
+        '
+        'txtScore
+        '
+        Me.txtScore.AutoSize = True
+        Me.txtScore.BackColor = System.Drawing.Color.Transparent
+        Me.txtScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtScore.ForeColor = System.Drawing.Color.White
+        Me.txtScore.Location = New System.Drawing.Point(102, 18)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(59, 31)
+        Me.txtScore.TabIndex = 4
+        Me.txtScore.Text = "000"
+        '
+        'startDelay
+        '
+        Me.startDelay.Interval = 1000
+        '
+        'player
+        '
+        Me.player.BackColor = System.Drawing.SystemColors.WindowText
+        Me.player.Image = CType(resources.GetObject("player.Image"), System.Drawing.Image)
+        Me.player.Location = New System.Drawing.Point(354, 503)
+        Me.player.Name = "player"
+        Me.player.Size = New System.Drawing.Size(59, 52)
+        Me.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.player.TabIndex = 1
+        Me.player.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(552, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
+        'lblGameOver
+        '
+        Me.lblGameOver.AutoSize = True
+        Me.lblGameOver.BackColor = System.Drawing.Color.Transparent
+        Me.lblGameOver.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGameOver.ForeColor = System.Drawing.Color.White
+        Me.lblGameOver.Location = New System.Drawing.Point(307, 220)
+        Me.lblGameOver.Name = "lblGameOver"
+        Me.lblGameOver.Size = New System.Drawing.Size(177, 31)
+        Me.lblGameOver.TabIndex = 6
+        Me.lblGameOver.Text = "GAME OVER"
+        Me.lblGameOver.Visible = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(812, 587)
+        Me.Controls.Add(Me.lblGameOver)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dbg)
+        Me.Controls.Add(Me.player)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Galaga"
+        CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents player As System.Windows.Forms.PictureBox
+    Friend WithEvents updateTimer As System.Windows.Forms.Timer
+    Friend WithEvents dbg As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtScore As System.Windows.Forms.Label
+    Friend WithEvents startDelay As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblGameOver As System.Windows.Forms.Label
+
+End Class
---
+++ Galaga/Main Form.vb
@@ -0,0 +1,332 @@
+﻿Public Class frmMain
+
+    ' Rewrite collision detection 
+    ' Finish dropping enemies
+    ' Find boss texture
+
+    Dim gameRunning As Boolean = False
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Play our starting sound
+        My.Computer.Audio.Play(My.Resources.galaga_levelstart, AudioPlayMode.Background)
+
+        startDelay.Start()
+    End Sub
+
+    Private Sub startDelay_Tick(sender As Object, e As EventArgs) Handles startDelay.Tick
+        ' Timer has ticked, start the game
+        gameStart()
+        startDelay.Stop()
+    End Sub
+
+    Dim enemies = New List(Of enemy)
+    Dim bullets = New List(Of playerBullet)
+    Dim score As Integer = 0
+    Private Sub gameStart()
+        gameRunning = True
+
+        ' Reset the bullets and enemies
+        ReDim bullets(1)
+        ReDim enemies(1)
+        score = 0
+
+        ' Add enemies
+        Dim createdEnemies = 0
+        Dim xBuffer = 70
+        Dim yBuffer = 60
+        Dim type = "red"
+        Dim row As Integer = 1
+        For index As Integer = 1 To 40
+            Dim pos = New Point(xBuffer, yBuffer)
+
+            ' Create a new enemy
+            Dim newEnemy = New enemy(player, pos, type)
+            Me.Controls.Add(newEnemy)
+
+            ' Expand the array
+            ReDim Preserve enemies(enemies.Length + 1)
+
+            ' Add the new enemy
+            enemies(createdEnemies) = newEnemy
+
+            ' Class variables
+            newEnemy.row = row
+            newEnemy.originalLocation = newEnemy.Location
+            newEnemy.type = type
+            newEnemy.id = index
+
+            ' Every other row moves the same direction
+            If newEnemy.row Mod 2 = 0 Then
+                newEnemy.moveLeft = Not newEnemy.moveLeft
+            End If
+
+            createdEnemies += 1
+            xBuffer += newEnemy.Size.Width + 30
+
+            ' Every 10 enemies, create a new row
+            If index Mod 10 = 0 Then
+                row += 1
+
+                ' Every 2 rows, switch color
+                If index Mod 20 = 0 Then
+                    If type = "red" Then
+                        type = "blue"
+                    Else
+                        type = "red"
+                    End If
+                End If
+
+                yBuffer += newEnemy.Size.Height + 20
+                xBuffer = 70
+            End If
+        Next
+    End Sub
+
+    Dim playerVelocity As Double = 0
+    Dim time As Double = 0
+    Dim nextMove As Double = 0
+    Dim nextDrop As Double = 0
+    Dim nextGC As Integer = 0
+    Private Sub gameUpdate()
+        ' Update the player location and velocity
+        playerVelocity = playerVelocity * 0.9
+        Dim pX = player.Location.X + playerVelocity
+
+        ' Move the player
+        player.Location = New Point(pX, player.Location.Y)
+
+        ' Clamp the player's location
+        If player.Location.X <= 0 Then
+            player.Location = New Point(0, player.Location.Y)
+        ElseIf player.Location.X + player.Size.Width >= Me.Width Then
+            player.Location = New Point(Me.Width - player.Size.Width, player.Location.Y)
+        End If
+
+        ' Check if bullets have hit something
+        For Each bullet As playerBullet In bullets
+            If Not IsNothing(bullet) Then
+                ' debug.Text = bullet.Location.Y
+                If bullet.Location.Y + bullet.Size.Height <= 0 Then
+                    bullet = Nothing
+                Else
+                    bullet.Location = New Point(bullet.Location.X, bullet.Location.Y - 10)
+                End If
+
+                For Each enemy As enemy In enemies
+                    If Not IsNothing(enemy) And Not IsNothing(bullet) Then
+                        If Not enemy.dead And Not bullet.dead Then
+                            If bullet.Location.X >= enemy.Location.X And bullet.Location.X + bullet.Size.Width <= enemy.Location.X + enemy.Size.Width Then
+                                If bullet.Location.Y <= enemy.Location.Y + enemy.Size.Height And bullet.Location.Y >= enemy.Location.Y Then
+                                    enemy.dead = True
+                                    bullet.dead = True
+
+                                    If enemy.type.ToLower() = "red" Then
+                                        score += 80
+                                    Else
+                                        score += 50
+                                    End If
+
+                                    txtScore.Text = score
+
+                                    My.Computer.Audio.Play(My.Resources.galaga_death1, AudioPlayMode.Background)
+
+                                    Me.Controls.Remove(enemy)
+                                    enemy.Dispose()
+
+                                    Me.Controls.Remove(bullet)
+                                    bullet.Dispose()
+                                End If
+                            End If
+                        End If
+                    End If
+                Next
+            End If
+        Next
+
+        For Each enemy As enemy In enemies
+            If Not IsNothing(enemy) Then
+                If enemy.Location.X >= player.Location.X And enemy.Location.X + enemy.Size.Width <= player.Location.X + player.Size.Width Then
+                    If enemy.Location.Y <= player.Location.Y + player.Size.Height And enemy.Location.Y >= player.Location.Y Then
+                        enemy.dead = True
+
+                        Me.Controls.Remove(enemy)
+                        enemy.Dispose()
+
+                        Me.Controls.Remove(player)
+                        player.Dispose()
+
+                        lblGameOver.Visible = True
+
+                        gameRunning = False
+                    End If
+                End If
+            End If
+        Next
+
+        ' Move the enemies
+        If time > nextMove Then
+            For Each enemy As enemy In enemies
+                If Not IsNothing(enemy) Then
+                    If Not enemy.dead And Not enemy.dropping Then
+                        Dim x As Integer = enemy.Location.X
+                        Dim y As Integer = enemy.Location.Y
+
+                        If enemy.moveLeft = True Then
+                            enemy.Location = New Point(enemy.Location.X - 5, enemy.Location.Y)
+                        Else
+                            enemy.Location = New Point(enemy.Location.X + 5, enemy.Location.Y)
+                        End If
+
+                        ' Clamp how far left or right they can go
+                        If enemy.Location.X - enemy.originalLocation.X > 20 Or enemy.originalLocation.X - enemy.Location.X > 20 Then
+                            enemy.moveLeft = Not enemy.moveLeft
+                        End If
+                    ElseIf enemy.dropping Then
+                        Dim x = enemy.Location.X + (Math.Cos(enemy.Location.Y / 200) * 50)
+                        Dim y = enemy.Location.Y + 10
+
+                        enemy.Location = New Point(x, y)
+
+                        If enemy.Location.Y > Me.Height Then
+                            enemy.dead = True
+                            enemy.dropping = False
+                        End If
+                    End If
+                End If
+            Next
+            nextMove = time + 1
+        End If
+
+        If time > nextDrop Then
+            For i As Integer = 0 To 40 Step 10
+                If i <> 0 And Not IsNothing(enemies(i)) Then
+                    Dim enemy As enemy = enemies(i)
+
+                    If Not enemy.dead Then
+                        enemy.dropping = True
+                        Exit For
+                    End If
+                End If
+            Next
+
+            nextDrop = time + 5
+        End If
+
+        ' Garbage collection - Remove all 'dead' entities from the list
+        If time > nextGC Then
+            Dim newEnemies = New List(Of enemy)
+            Dim newBullets = New List(Of playerBullet)
+
+            ' Enemies 
+            For Each enemy As enemy In enemies
+                If Not IsNothing(enemy) Then
+                    If Not enemy.dead Then
+                        newEnemies.Add(enemy)
+                    End If
+                End If
+            Next
+
+            enemies = newEnemies
+
+            ' Bullets 
+            For Each bullet As playerBullet In bullets
+                If Not IsNothing(bullet) Then
+                    If Not bullet.dead Then
+                        newBullets.Add(bullet)
+                    End If
+                End If
+            Next
+
+            bullets = newBullets
+
+            nextGC = time + 5
+        End If
+    End Sub
+
+    ' Detect key pressing
+    Private Sub frmMain_KeyPress(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar.ToString() = "a" Then
+            playerVelocity = -10
+        ElseIf e.KeyChar.ToString() = "d" Then
+            playerVelocity = 10
+        End If
+    End Sub
+
+    Dim createdShots = 0
+    Private Sub shootBullet()
+        If gameRunning = False Then
+            Return
+        End If
+
+        ' Create a new bullet
+        Dim newShot = New playerBullet(player)
+        Me.Controls.Add(newShot)
+
+        ' Expand the array
+        'ReDim Preserve bullets(bullets.Length + 1)
+
+        ' Add the new bullet
+        bullets.Add(newShot)
+        'bullets(createdShots) = newShot
+
+        My.Computer.Audio.Play(My.Resources.galaga_shooting, AudioPlayMode.Background)
+
+        createdShots += 1
+    End Sub
+
+    Private Sub background_Click(sender As Object, e As EventArgs) Handles Me.Click
+        shootBullet()
+    End Sub
+
+    Private Sub updateTimer_Tick(sender As Object, e As EventArgs) Handles updateTimer.Tick
+        time += 0.1
+
+        If gameRunning = True Then
+            gameUpdate()
+        End If
+    End Sub
+End Class
+
+' The player's bullet class
+Public Class playerBullet
+    Inherits PictureBox
+
+    Public dead As Boolean = False
+
+    ' Constructor
+    Public Sub New(ByVal player As PictureBox)
+        Image = My.Resources.galaga_bullet
+        SizeMode = PictureBoxSizeMode.StretchImage
+        Size = New Size(17, 35)
+        BackColor = Color.Black
+        Location = New Point(player.Location.X + player.Size.Width / 2 - Size.Width / 2, player.Location.Y)
+    End Sub
+
+End Class
+
+' Enemy class
+Public Class enemy
+    Inherits PictureBox
+
+    Public type As String
+    Public row As Integer
+    Public id As Integer
+    Public dead As Boolean = False
+    Public dropping As Boolean = False
+
+    Public originalLocation As Point
+    Public moveLeft As Boolean = False
+
+    ' Constructor
+    Public Sub New(ByVal player As PictureBox, ByVal pos As Point, ByVal type As String)
+        If type.ToLower() = "red" Then
+            Image = My.Resources.galaga_enemy1
+        Else
+            Image = My.Resources.galaga_enemy2
+        End If
+        SizeMode = PictureBoxSizeMode.StretchImage
+        Size = New Size(40, 40)
+        BackColor = Color.Black
+        Location = New Point(pos)
+    End Sub
+
+End Class
